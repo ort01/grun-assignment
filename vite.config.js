cff1600ab7a512a2b88c970dda23e6bfe7e1cb35
ort01 <ortutova.kristina@gmail.com>
2023-07-15 18:16:46 +0200

    animation

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/variables.scss"; @import "./src/global.scss";`
+        additionalData: `@import "./src/variables.scss"; @import "./src/global.scss"; @import "./src/animations.scss";`
       }
     }
   },
